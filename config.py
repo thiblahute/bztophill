117a00e7a3c9b08506caf3064df8b59060e66012
Thibault Saunier <tsaunier@gnome.org>
2015-08-09 12:00:54 +0200

    gst: Add GStreamer config file

---
+++ config.py
@@ -0,0 +1,30 @@
+project_ids = {
+    "GStreamer": "gstreamer",
+    "gstmm": "GST_MM",
+    "gnonlin": "GNL",
+    "documentation": "GSTDOCUMENTATION",
+    "common": "GSTCOMMON",
+    "gstreamer (core)": "GST_CORE",
+}
+
+
+project_names = {
+    "gstreamer (core)": "GStreamer Core",
+    "gst-devtools": "GStreamer Development Tools",
+    "gst-editing-services": "GStreamer Editing Services",
+    "gst-libav": "GStreamer Libav",
+    "gst-plugins-bad": "GStreamer Plugins Bad",
+    "gst-plugins-base": "GStreamer Plugins Base",
+    "gst-plugins-good": "GStreamer Plugins Good",
+    "gst-plugins-ugly": "GStreamer Plugins Ugly",
+}
+
+
+emails_uname = {
+    "nicolas.dufresne": "nicolas.dufresne@collabora.co.uk",
+    "nicolas.dufresne": "nicolas.dufresne@collabora.com",
+}
+
+people_who_do_not_want_an_account = {
+    "denkpadje@gmail.com": "denkpadje",
+}
